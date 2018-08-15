20245802401d05e830b873881ca9bacfc48774b4
nagasudhirpulla <nagasudhirpulla@gmail.com>
2018-08-15 23:25:39 +0530

    small changes

---
+++ app.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var router = express.Router();
+var multer = require('multer');
+var bodyParser = require('body-parser');
+var fs = require('fs');
+
+// configure multer diskStorage
+var storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'public')
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.originalname)
+    }
+});
+
+var upload = multer({ storage: storage });
+
+// listen to the post request and also use the multer middleware to get the uploaded file info
+router.post('/upload', upload.single('file'), (req, res, next) => {
+    // copy the uploaded file to the desired destination which is supplied in the request body by dest field
+    fs.copyFile(req.file.path, req.body.dest + '\\' + req.file.originalname, err => {
+        if (err) {
+            console.error(err);
+            return res.json({ 'message': 'upload failed', 'error': err });
+        }
+        res.json({ 'message': 'File uploaded successfully', 'error': err });
+    });
+});
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(router);
+app.listen(3002, () => console.log('File upload app listening on port 3002!'))
---
+++ restart_server.bat
@@ -0,0 +1,1 @@
+pm2 restart file_upload_server
